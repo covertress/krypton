67add4670574a3965d9d53bb4ea48caaaa32b7ee
krypton-dev <krypton-dev>
2016-09-20 17:54:52 +0000

    Extend PoW to block 20160

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -95,7 +95,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        nLastPOWBlock = 10080;
+        nLastPOWBlock = 20160;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
